560a59755922a74623c26072a3fec01cfd5da4a7
ecmascriptguru <ecmascript.guru@gmail.com>
2017-06-02 03:30:58 -0400

    switched to newer design

--- js/background.js
+++ js/background.js
@@ -1,9 +1,15 @@
 var reordering = false;
-var animation = new iconAnimator("images/icon.png");
+var animation = new iconAnimator("images/mini.png");
 animation.set();
 
 chrome.runtime.onMessage.addListener(
 function(request, sender, sendResponse) {
+    if (request.main_action == 'show_window') {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {main_action: request.main_action});
+        });
+    }
+
     if (request.main_action == 'reorder') {
         reordering = true;
         startAnimation();
--- js/content.js
+++ js/content.js
@@ -1,153 +1,131 @@
-let loadRepins = (function() {
-
-    let pages = 5;
-    let count = 0;
-    let mylist = [];
-    let timeout;
-
-    function load() {
-        $('.Pin').each(function() {
-            mylist.push($(this)[0])
-        });
-
-        if (pages > 0) {
-
-            $(window).scrollTop($(document).height());
-
-            timeout = window.setTimeout(load, 4000);
-
-            count++;
-            if (count == pages) {
-                finish();
-            }
-
-        } else {
-            finish();
-        }
-    }
-
-    function getColsCount() {
-        let width = $(".Pin").parents("._4e").innerWidth(),
-            postWidth = 236,
-            horGap = 24;
-
-        return (width + horGap) / (postWidth + horGap);
-    }
-
-    function finish() {
-        let colsCount = getColsCount(),
-            positions = [];
-
-        for (let i = 0; i < colsCount; i ++) {
-            positions.push({
-                left: 0,
-                top: 0
-            })
-        }
-
-        for (let i = 1; i < positions.length; i ++) {
-            positions[i].left = (236 + 24) * i;
-        }
-
-        window.clearTimeout(timeout);
-
-        let list = mylist.filter(function(f) {
-            if ($(f).find('.repinIconSmall').length > 0) {
-                return f;
-            }
-        });
-
-        list.sort(function(a, b) {
-            let compAText = $(a).find('.repinIconSmall').next().text().trim(),
-                compBText = $(b).find('.repinIconSmall').next().text().trim();
-            let compA = turnK(compAText);
-            let compB = turnK(compBText);
-            return (compA == compB) ? 0 : (compA > compB) ? -1 : 1;
-        });
-
-        if ($(".Grid").length) {
-            $(".Grid").before('<div id="organized" style="margin:0 12px;"></div>');
-        } else {
-            $("._4e.relative").before('<div id="organized" style="margin:0 12px;"></div>');
-        }
-
-        let setPosition = (itm) => {
-            let minTop = Number.POSITIVE_INFINITY,
-                minIndex = 0;
-            
-            for (let i = 0; i < positions.length; i ++) {
-                if (positions[i].top < minTop) {
-                    minTop = positions[i].top;
-                    minIndex = i;
-                }
-            }
-            $(itm).css({
-                top: 0,
-                left: 0,
-                transform: `translateX(${positions[minIndex].left}px) translateY(${positions[minIndex].top}px)`,
-                width: "236px",
-                position: "absolute"
-            });
-            positions[minIndex].top += ($(itm).height() + 24);
-        }
-
-
-        $.each(list, function(idx, itm) {
-            $("#organized").append($(itm));
-            setPosition(itm);
-        });
-
-        if ($(".Grid").length) {
-            $(".Grid").remove();
-        } else {
-            $("._4e.relative").remove();
-            $("#organized").addClass("_4e relative");
-            // $(".gridCentered").remove();
-        }
-
-        let containerHeight = 0;
-        positions.forEach((pos) => {
-            if (containerHeight < pos.top) {
-                containerHeight = pos.top;
-            }
-        });
-        $("#organized").height(containerHeight);
-        reorderIsDone();
-    }
-
-    function turnK(text) {
-        let number = Number(text.replace(/k/g, ''));
-        if (text.indexOf("k") !== -1) {
-            number = number * 1000;
-        }
-        return number;
-    }
-
-    function resetValues(pageCount, initialCount, list) {
-        pages = pageCount;
-        count = initialCount;
-        mylist = list;
-        clearTimeout(timeout);
-    }
-
-    function reorderIsDone() {
-        $('#loading-page-merchpins').remove();
-        $("html, body").scrollTop(0);
-        chrome.runtime.sendMessage({main_action: 'reorder_done'});
-    }
-
-    return function(page) {
-        resetValues(page, 0, []);
-        $('body').append('<div id="loading-page-merchpins" style="opacity: 0.4; background: black; position: fixed; top: 0px; left: 0px; right: 0px; bottom: 0px; text-align: center;"><img style="position: fixed; top: 45%; margin-left:-40px;" width="80px" src="'+chrome.extension.getURL('images/loader.svg')+'" /></div>');
-        load();
-    }
-
-}());
+var interval = null;
+var currentHeight = null;
 
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.main_action == 'reorder') {
-        loadRepins(msg.number_pages);
+        startReordering(msg.number_pages);
+    }
+    if (msg.main_action == 'show_window') {
+        if ($('.merchipins-window').length > 0) {
+            $('.merchipins-window').remove();
+        } else {
+            $('body').append('<img class="merchipins-window" style="cursor: pointer;  z-index: 999999; border: none; position: fixed; top: 11px; right: 21px; " src="'+chrome.extension.getURL('images/close.png')+'" />');
+            $('body').append('<iframe class="merchipins-window" id="merchipins-window" style="background: #999; box-shadow: 1px 1px 5px #888888; border: 1px solid #CCC; z-index: 99999; border: none; position: fixed; top: 20px; right: 30px; width:350px; height: 270px;" src="'+chrome.extension.getURL('html/browser_action.html')+'" />')
+
+            $('img.merchipins-window').click(function(){
+                $('.merchipins-window').remove();
+            });
+        }
     }
 });
 
-    
+function startReordering(pages) {
+    $('.merchipins-window').remove();
+    $('body').append('<div id="loading-page-merchpins" style="opacity: 0.4; background: black; position: fixed; top: 0px; left: 0px; right: 0px; bottom: 0px; text-align: center;"><img style="position: fixed; top: 45%; margin-left:-40px;" width="80px" src="'+chrome.extension.getURL('images/loader.svg')+'" /></div>')
+    currentHeight = $('.variableHeightLayout').height();
+
+    interval = setInterval(function(){
+        var tempHeight = $('.variableHeightLayout').height();
+        if (currentHeight != tempHeight) {
+            currentHeight = tempHeight;
+
+            pages--;
+            if (pages > 0) {
+                oneMorePage();
+            } else {
+                clearAndFinish();
+            }
+        }
+
+        if ($('.gridFooterLogoIcon').is(":visible")) {
+            clearAndFinish();
+        }
+    }, 100);
+
+    oneMorePage();
+}
+
+function clearAndFinish() {
+    clearInterval(interval);
+    reorderResults();
+    reorderIsDone();
+}
+
+function oneMorePage() {
+    $("html, body").scrollTop(document.body.clientHeight);
+}
+
+function compare(a,b) {
+  if (a.num < b.num)
+    return -1;
+  if (a.num > b.num)
+    return 1;
+  return 0;
+}
+
+function reorderResults() {
+
+    var leftColumns = {};
+    var pins = [];
+    $('.item').each(function() {
+        leftColumns[$(this).position().left] = 1;
+
+        $('.visuallyHidden').remove();
+        var temp = $.trim($(this).find('.repinCountSmall').text()).replace(/k/g, '000');
+        if (temp.indexOf('.') > 0 || temp.indexOf(',') > 0) {
+            temp = temp.replace('0', ''); // remove one zero
+        }
+
+        var pinCount = parseInt(temp.replace(/,/g, '').replace(/\./g,''));
+
+        pins.push({num: pinCount, html: $(this).html()});
+    });
+    numberColumns = Object.keys(leftColumns).length;
+
+    var item = 0;
+    pins.sort(compare).reverse().forEach(function(one) {
+        var el = $('.item').eq(item);
+        el.html(one.html);
+        item++;
+    });
+
+
+    item = 0;
+    var heights = [];
+    var biggestHeight = 0;
+    var totalheight = 0;
+    $('.item').each(function(){
+        
+        $(this).css({'top' : totalheight + 'px'});
+        $(this).css({'left': Object.keys(leftColumns)[heights.length] + 'px'});
+
+        heights.push($(this).find('img').height()+$(this).find('.pinMetaWrapper').outerHeight()+50);
+        $(this).height(getMax(heights));
+
+        if (heights.length == numberColumns) {
+            biggestHeight = getMax(heights);
+
+            for (var i=0; i<numberColumns; i++) {
+                $('.item').eq(item-i).height(biggestHeight);
+            }
+
+            totalheight += $('.item').eq(item).outerHeight();
+
+            heights = [];
+        }
+
+        item++;
+    });
+
+    $("html, body").scrollTop(0);
+  
+}
+
+function getMax(arr) {
+    return Math.max.apply(Math, arr)
+}
+
+function reorderIsDone() {
+    $('#loading-page-merchpins').remove();
+    chrome.runtime.sendMessage({main_action: 'reorder_done'});
+}
